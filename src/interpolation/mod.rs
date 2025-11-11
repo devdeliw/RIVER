bdbd8ada0ab590d31b96e00582cb7c8e99d76ffe
devdeliw <devaldeliwala@berkeley.edu>
2025-11-10 19:43:35 -0800

    reexports

--- src/interpolation/mod.rs
+++ src/interpolation/mod.rs
@@ -8,3 +8,7 @@ pub use traits::Interpolator;
 pub mod linear; 
 pub mod newton; 
 pub mod spline;
+
+pub use spline::natural   as natural_spline; 
+pub use spline::clamped   as clamped_spline; 
+pub use spline::monotonic as monotonic_spline;
